c18869c521a608c750e4343dbada428597478b24
hemadeepika421 <156604727+hemadeepika421@users.noreply.github.com>
2024-07-15 13:03:27 +0530

    Delete citation directory

--- citation/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import sindhi,northeast,malayalam,contact,language,literary_work
-# Register your models here.
-admin.site.register(sindhi)
-admin.site.register(northeast)
-admin.site.register(malayalam)
-admin.site.register(contact)
-admin.site.register(language)
-admin.site.register(literary_work)
